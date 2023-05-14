84a0041fa20885fa8573c6e6561b27ced0262172
Ramazan <109206156+frogy9cat@users.noreply.github.com>
2023-05-14 23:38:43 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,11 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
-from keyboards.inline import InlineAbout
+from keyboards.inline.InlineAbout import Sharing
 
 from loader import dp
 
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    await message.answer(f"Бот для инлайн поиска информации о погоде в городах и странах", reply_markup=InlineAbout)
+    text = f"Бот для инлайн поиска информации о погоде в городах и странах"
+    await message.reply(text=text, reply_markup=Sharing)
     
