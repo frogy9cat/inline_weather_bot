962f657affcc5763dde15a047d34659718a962ff
Ramazan <109206156+frogy9cat@users.noreply.github.com>
2023-05-14 23:03:44 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -8,3 +8,4 @@ from loader import dp
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     await message.answer(f"Бот для инлайн поиска информации о погоде в городах\странах.\n\nСоздатель: @frogy_cat", reply_markup=InlineAbout)
+    
