d7346984671f89f4ca3e29670925339a4ea279a9
Ramazan <109206156+frogy9cat@users.noreply.github.com>
2023-05-14 23:02:51 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,10 +1,3 @@
-# import os
-
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
-# ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
-# IP = str(os.environ.get("ip"))  # Xosting ip manzili
-
 
 BOT_TOKEN='{BOT TOKEN}'
 ADMINS=['{USER ID}']
