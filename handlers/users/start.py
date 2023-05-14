0a0c8fb73a81dcc7f3d7f01d1d8eebd50b06eeb4
Ramazan <109206156+frogy9cat@users.noreply.github.com>
2023-05-14 23:32:54 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,4 +6,4 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer(f"Привет! Этот бот предоставляет возможность получить информацию о погоде. Всё, что нужно сделать, это лишь написать юзер этого бота, а затем название города или страны.")
